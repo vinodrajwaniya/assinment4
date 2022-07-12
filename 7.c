d706e948971fe3ff967c761c38fc79f628ea5d66
vinodrajwaniya <72007075+vinodrajwaniya@users.noreply.github.com>
2022-07-12 12:33:27 +0530

    Add files via upload

---
+++ 7.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int myInt=10;
+    char myChar='A';
+    float myFloat=16.22;
+    printf("integer value is %d\nCharacter value is %c\nFloat value is %f",myInt,myChar,myFloat);
+
+
+    getch();
+}
